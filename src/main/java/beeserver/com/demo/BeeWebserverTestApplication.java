69722fde9a18bb18aafb92f0741e4d70c3e0a731
samtrobertson <samrobertson@nevada.unr.edu>
2020-12-26 13:10:11 -0800

    spring boot testing

---
+++ src/main/java/beeserver/com/demo/BeeWebserverTestApplication.java
@@ -0,0 +1,13 @@
+package beeserver.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeWebserverTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeWebserverTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beeserver/com/demo/BeeWebserverTestApplicationTests.java
@@ -0,0 +1,13 @@
+package beeserver.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeWebserverTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
