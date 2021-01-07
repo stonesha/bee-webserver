f2a6c46f6349ca69fac472cae32d53f502fad5d7
Chris Diewtragulchai <44313901+Diewt@users.noreply.github.com>
2021-01-07 12:38:51 -0800

    Database connection test using heroku github example

--- src/main/java/com/Bee/BeeWebserver/BeeWebserverApplication.java
+++ src/main/java/com/Bee/BeeWebserver/BeeWebserverApplication.java
@@ -1,10 +1,24 @@
 package com.Bee.BeeWebserver;
 
+//basic Web application import
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//dependencies for the rest controller
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+//Test dependencies for database connection
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikeri.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Bean;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 @SpringBootApplication
 public class BeeWebserverApplication {
@@ -18,6 +32,12 @@ public class BeeWebserverApplication {
 @RestController
 class HelloController{
 
+	@Value("spring.datasource.url")
+	private String dbUrl;
+
+	@Autowired
+	private DataSource dataSource;
+
 	@GetMapping("/")
 	String hello(){
 		return "Hello World";
@@ -25,6 +45,30 @@ class HelloController{
 
 	@GetMapping("/test")
 	String test(){
-		return "PeePeePooPoo";
+		try (Connection connection = dataSource.getConnection()) 
+		{
+			return "PeePeePooPoo";
+		} 
+		catch(Exception e) 
+		{
+			return "Error";
+		}
+	}
+
+	@Bean
+	public DataSource dataSource() throws SQLException
+	{
+		if(dbUrl == null || dbUrl.isEmpty())
+		{
+			return new HikariDataSource();
+		}
+		else
+		{
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
 	}
 }
+
+
