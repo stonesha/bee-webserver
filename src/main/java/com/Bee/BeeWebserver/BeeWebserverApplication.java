a6a80b994061044f87422862424250bdb0eabdbe
Chris Diewtragulchai <44313901+Diewt@users.noreply.github.com>
2021-01-14 13:12:36 -0800

    Update BeeWebserverApplication.java

--- src/main/java/com/Bee/BeeWebserver/BeeWebserverApplication.java
+++ src/main/java/com/Bee/BeeWebserver/BeeWebserverApplication.java
@@ -46,7 +46,7 @@ public class BeeWebserverApplication {
 		try (Connection connection = dataSource.getConnection()) 
 		{
 			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("INSERT INTO routes VALUES (safe)");
+			stmt.executeUpdate("INSERT INTO routes (status) VALUES (safe)");
 			return "PeePeePooPoo";
 		} 
 		catch(Exception e) 
