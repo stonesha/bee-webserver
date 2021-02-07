2336c9ef0f19bc0b6c1dae1bc4135767a2cbb0b6
Chris Diewtragulchai <44313901+Diewt@users.noreply.github.com>
2021-02-06 22:53:20 -0800

    Update BeeWebserverApplication.java
    testing to see if imports work

--- src/main/java/com/Bee/BeeWebserver/BeeWebserverApplication.java
+++ src/main/java/com/Bee/BeeWebserver/BeeWebserverApplication.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 //import org.springframework.jbdc.core.JbdcTemplate;
 
 //Test dependencies for database connection
