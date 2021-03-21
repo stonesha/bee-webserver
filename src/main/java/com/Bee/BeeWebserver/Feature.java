9c20420eb731262037d68da3f29d2790e35fc937
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-03-21 01:16:22 -0700

    Update Feature.java

--- src/main/java/com/Bee/BeeWebserver/Feature.java
+++ src/main/java/com/Bee/BeeWebserver/Feature.java
@@ -3,9 +3,9 @@ package com.Bee.BeeWebserver;
 public class Feature{
 
     public String type;
-    public float[] coordinates;
+    public float[][] coordinates;
 
-    public Feature(String type, float[] coordinates){
+    public Feature(String type, float[][] coordinates){
         this.type = type;
         this.coordinates = coordinates;
     }
