21626a6e32bbe4c08fdac008c2e956e7e54793fd
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-03-21 00:45:01 -0700

    Create Feature.java

---
+++ src/main/java/com/Bee/BeeWebserver/Feature.java
@@ -0,0 +1,12 @@
+package com.Bee.BeeWebserver;
+
+public class Evacuee{
+
+    public String type;
+    public float[] coordinates;
+
+    public Evacuee(String type, float[] coordinates){
+        this.type = type;
+        this.coordinates = coordinates;
+    }
+}
