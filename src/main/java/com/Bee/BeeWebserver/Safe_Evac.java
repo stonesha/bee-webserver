a6532934b14655b6bdcb8b90373209ee1bcbda9a
samtrobertson <samrobertson@nevada.unr.edu>
2021-04-24 13:39:02 -0700

    more json testing

--- src/main/java/com/Bee/BeeWebserver/Safe_Evac.java
+++ src/main/java/com/Bee/BeeWebserver/Safe_Evac.java
@@ -3,4 +3,9 @@ package com.Bee.BeeWebserver;
 public class Safe_Evac{
     public Integer total_evacuees;
     public Integer safe_evacuees;
+
+    public Safe_Evac(Integer total_evacuees, Integer safe_evacuees){
+        this.total_evacuees = total_evacuees;
+        this.safe_evacuees = safe_evacuees;
+    }
 }
